e2902db7ad3558be3d806b5fc1227fea2aead1e1
Brian Lane <brian@gourmetgaming.com>
2016-03-05 19:21:34 -0500

    Changed file extension to .java

--- codingBatSolutions.java
+++ codingBatSolutions.java
@@ -9,10 +9,10 @@ public int[] make2(int[] a, int[] b) {
 int[] res = new int[2];
 
 if ( a.length >= 2 )
-  {
-    res[0] = a[0];
-    res[1] = a[1];
-  }
+    {
+      res[0] = a[0];
+      res[1] = a[1];
+    }
 
   else if ( a.length == 1)
   {
